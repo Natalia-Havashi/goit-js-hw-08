8afb9fde154daa2f879759b61c3e9e048d1dc08e
Natalia-Havashi <natali.gavashi@gmail.com>
2023-04-17 17:17:30 +0300

    HOMEWORK 8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(time) {
+    localStorage.setItem('videoplayer-current-time', time.seconds);
+}
+
+const lastPayerTime = localStorage.getItem('videoplayer-current-time');
+
+if(lastPayerTime){
+    player.setCurrentTime(lastPayerTime);
+} else {
+    console.log(' ');
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from "lodash.throttle";
 
+const KEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+const { email, message } = form.elements;
+
+let dataForm = JSON.parse(localStorage.getItem(KEY)) || {};
+
+reloedPage();
+
+form.addEventListener('input', throttle(onInputData, 500));
+form.addEventListener('submit', onFormSubmit);
+
+function onInputData(e) {
+    dataForm = { email: email.value, message: message.value
+    };
+    localStorage.setItem(KEY, JSON.stringify(dataForm));
+
+}
+
+function reloedPage() {
+    if (dataForm) {
+        email.value = dataForm.email || '' ;
+        message.value = dataForm.message || '';
+    }
+}
+
+function onFormSubmit(e) {
+    e.preventDefault();
+
+    if(email.value === '' || message.value === '') {
+return alert(' ');
+    } else {
+        console.log({email: email.value, message: message.value});
+
+    }
+
+    localStorage.removeItem(KEY);
+e.currentTarget.reset();
+dataForm = {};
+}
+
+console.log(KEY);
